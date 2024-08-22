113fdc0609f5613d97f841fa8d56fd0cda52c3d4
nam-no <109804279+nam-no@users.noreply.github.com>
2024-08-23 00:41:11 +0700

    Update manageRegisTry.py

--- Python/manageRegisTry.py
+++ Python/manageRegisTry.py
@@ -1,149 +1,240 @@
-import subprocess
-from colorama import Fore,Style
-def list_type_value():
-    print("[+] what do you want typt data ? ")
-    print("[-] 1.REG_SZ (String)")
-    print("[-] 2.REG_EXPAND_SZ (Expandable String)")
-    print("[-] 3.REG_DWORD (32-bit Integer)")
-    print("[-] 4.REG_QWORD (64-bit Integer)")
-    print("[-] 5.REG_BINARY (Binary Data)")
-    print("[-] 6.REG_BINARY (Binary Data)")
+import ctypes
+from ctypes import wintypes
 
-def menu():
-    print("[+] What do you want to do ?")
-    print("[-] 1. Add the registry key .")
-    print("[-] 2. Add the registry value .")
-    print("[-] 3. Delete the registry key .")
-    print("[-] 4. Delete the registry value .")
+# Khai báo các hằng số cần thiết
+HKEY_CURRENT_USER = 0x80000001
+KEY_READ = 0x20019
+HKEY_LOCAL_MACHINE = 0x80000002
+KEY_WRITE = 0x20006
+REG_OPTION_NON_VOLATILE = 0x00000000
+REG_SZ = 1
+KEY_WOW64_64KEY = 0x0100
 
-def get_value_type(choose):
-    value_type = " "
-    if(choose == 1):
-        value_type = "REG_SZ"
-    elif(choose == 2):
-        value_type = "REG_EXPAND_SZ"
-    elif(choose == 3):
-        value_type = "REG_DWORD"
-    elif(choose == 4):
-        value_type = "REG_QWORD"
-    elif(choose == 5):
-        value_type = "REG_BINARY"
-    elif(choose == 6):
-        value_type = "REG_MULTI_SZ"
-    else :
-        print("[+] Please, Choose only from 1 to 6 .")
-    return value_type
-def check_registry_key_exists(key_path):
-    command =["reg","query",key_path]
-    try:
-        result = subprocess.run(command, capture_output=True, text=True)
-        if result.returncode == 0:
-            return True
-        else:
-            return False
-    except Exception as e:
-        print(f"An error occurred: {e}")
+# Load thư viện advapi32.dll để làm việc với registry
+advapi32 = ctypes.WinDLL('advapi32', use_last_error=True)
+
+#Hàm RegOpenKeyEx
+RegOpenKeyEx = advapi32.RegOpenKeyExW
+RegOpenKeyEx.argtypes = [
+    wintypes.HKEY,        # hKey
+    wintypes.LPCWSTR,     # lpSubKey
+    wintypes.DWORD,       # ulOptions
+    wintypes.DWORD,      # samDesired
+    ctypes.POINTER(wintypes.HKEY)  # phkResult
+]
+#Hàm RegQueryValueEx
+RegQueryValueEx = advapi32.RegQueryValueExW
+RegQueryValueEx.argtypes = [
+    wintypes.HKEY,        # hKey
+    wintypes.LPCWSTR,     # lpValueName
+    wintypes.LPVOID,      # lpReserved
+    ctypes.POINTER(wintypes.DWORD), # lpType
+    wintypes.LPBYTE,      # lpData
+    ctypes.POINTER(wintypes.DWORD)  # lpcbData
+]
+# Định nghĩa các hàm cần thiết từ advapi32.dll
+RegCreateKeyEx = advapi32.RegCreateKeyExW
+RegSetValueEx = advapi32.RegSetValueExW
+RegCloseKey = advapi32.RegCloseKey
+
+
+# Định nghĩa kiểu dữ liệu cho các hàm
+RegCreateKeyEx.argtypes = [
+    wintypes.HKEY,        # hKey
+    wintypes.LPCWSTR,     # lpSubKey
+    wintypes.DWORD,       # Reserved
+    wintypes.LPWSTR,      # lpClass
+    wintypes.DWORD,       # dwOptions
+    wintypes.DWORD,        # samDesired
+    wintypes.LPVOID,      # lpSecurityAttributes
+    wintypes.PHANDLE,     # phkResult
+    wintypes.LPDWORD      # lpdwDisposition
+]
+
+RegSetValueEx.argtypes = [
+    wintypes.HKEY,        # hKey
+    wintypes.LPCWSTR,     # lpValueName
+    wintypes.DWORD,       # Reserved
+    wintypes.DWORD,       # dwType
+    wintypes.LPCWSTR,     # lpData
+    wintypes.DWORD        # cbData
+]
+# Định nghĩa kiểu dữ liệu cho hàm RegDeleteKeyEx
+RegDeleteKeyEx = advapi32.RegDeleteKeyExW
+RegDeleteKeyEx.argtypes = [
+    wintypes.HKEY,        # hKey
+    wintypes.LPCWSTR,     # lpSubKey
+    wintypes.DWORD,      # samDesired
+    wintypes.DWORD        # Reserved
+]
+# Định nghĩa kiểu dữ liệu cho hàm RegDeleValue
+RegDeleteValue = advapi32.RegDeleteValueW
+RegDeleteValue.argtypes = [
+    wintypes.HKEY,        # hKey
+    wintypes.LPCWSTR      # lpValueName
+]
+RegCloseKey.argtypes = [
+    wintypes.HKEY         # hKey
+]
+# Hàm kiểm tra sự tồn tại của một khóa Registry
+def check_registry_key(hkey, subkey):
+    handle = wintypes.HKEY()
+    result = RegOpenKeyEx(hkey, subkey, 0, KEY_READ, ctypes.byref(handle))
+
+    if result == 0:  # ERROR_SUCCESS
+        RegCloseKey(handle)
+        return True
+    else:
         return False
-def remove_registry_key(key_path):
-    command = ["reg",'delete',key_path,"/f"]
-    try:
-        result = subprocess.run(command, capture_output=True, text=True)
-        print(Fore.GREEN + "[+] Removed registry key successfully."+ Style.RESET_ALL)
-    except subprocess.CalledProcessError as e:
-        print(f"Error : {e}")
-def remove_registry_value(key_path,value_name):
-    command = ["reg", 'delete', key_path, "/v",value_name,"/f"]
-    try:
-        result = subprocess.run(command, capture_output=True, text=True)
-        print(Fore.GREEN + f"[+] Removed registry a value {value_name} of the  {key_path} successfully."+ Style.RESET_ALL)
-    except subprocess.CalledProcessError as e:
-        print(f"Error : {e}")
+# Hàm kiểm tra sự tồn tại của một giá trị trong khóa Registry
+def check_registry_value(hkey, subkey, value_name):
+    handle = wintypes.HKEY()
+    result = RegOpenKeyEx(hkey, subkey, 0, KEY_READ, ctypes.byref(handle))
+    if result != 0:  # Khóa không tồn tại
+        return False
+    data_type = wintypes.DWORD()
+    data_size = wintypes.DWORD()
+    result = RegQueryValueEx(handle, value_name, None, ctypes.byref(data_type), None, ctypes.byref(data_size))
+    RegCloseKey(handle)
+    if result == 0:  # ERROR_SUCCESS
+        return True
+    else:
+        return False
 
-def add_registry_key(key_path):
-     command = ["reg", "add", key_path]
-     try:
-         result = subprocess.run(command, capture_output=True, text=True)
-         if(check_registry_key_exists(key_path)==True):
-             print(Fore.GREEN + "[+] Key added successfully." + Style.RESET_ALL)
-         else:
-             print(Fore.RED + "[+] Invalid key path." + Style.RESET_ALL)
-     except subprocess.CalledProcessError as e:
-         print(f"Error creating task: {e}")
 
-def add_registry_value(key_path,value_name,value_data,value_type):
-    command =["reg","add",key_path,"/v",value_name,"/t",value_type,"/d",value_data]
-    try:
-        result = subprocess.run(command, capture_output=True, text=True)
-        print(Fore.GREEN +"[+] Key added successfully."+ Style.RESET_ALL)
-    except subprocess.CalledProcessError as e:
-        print(f"Error creating task: {e}")
+def create_registry_key(hkey, subkey):
+    handle = wintypes.HKEY()
+    disposition = wintypes.DWORD()
 
-def add():
-    key_path = input("[+] Enter the key path : ")
-    while (check_registry_key_exists(key_path) == True):
-        print("[-] Key path already  exist.")
-        key_path = input("[+] Enter the key path again : ")
-    value_name = input("[+] Enter the name of the value : ")
-    value_data = input("[+] Enter the data of the value : ")
-    list_type_value()
-    choose = int(input("Choose type value : "))
-    value_type = get_value_type(choose)
-    print(f"[+] Chose the type of the value is {value_type}")
-    add_registry_value(key_path, value_name, value_data, value_type)
-def delete():
-    key_path = input("[+] Enter the key path need to create : ")
-    while (check_registry_key_exists(key_path) == False):
-        print(Fore.RED + "[-] Key path does not  exist."+ Style.RESET_ALL)
-        key_path = input("[+] Enter the key path again : ")
+    result = RegCreateKeyEx(
+        hkey,
+        subkey,
+        0,
+        None,
+        REG_OPTION_NON_VOLATILE,
+        KEY_WRITE,
+        None,
+        ctypes.byref(handle),
+        ctypes.byref(disposition)
+    )
+    if result == 0:  # ERROR_SUCCESS
+        print(f"[+] Created '{subkey}' successfully.")
+        RegCloseKey(handle)
+        return True
+    else:
+        print(f"[+] Error {result}")
+        return False
 
-def run():
-    while (True):
-        menu()
-        chose = int(input("[+] Please, choose :"))
-        if(chose ==1):
-            print("[+] Chose mode 1. ")
-            key_path = input("[+] Enter the key path : ")
-            while (check_registry_key_exists(key_path) == True):
-                print(Fore.RED + "[-] Key path already  exist." +Style.RESET_ALL )
-                key_path = input("[+] Enter the key path again : ")
-            add_registry_key(key_path)
-        elif(chose==2):
-            print("[+] Chose mode 2. ")
-            key_path = input("[+] Enter the key path : ")
-            while (check_registry_key_exists(key_path) == False):
-                print(Fore.RED + "[-] Key path does not  exist."+ Style.RESET_ALL)
-                key_path = input("[+] Enter the key path again : ")
-            value_name = input("[+] Enter the name of the value : ")
-            value_data = input("[+] Enter the data of the value : ")
-            list_type_value()
-            choose = int(input("Choose type value : "))
-            value_type = get_value_type(choose)
-            print(f"[+] Chose the type of the value is {value_type}")
-            add_registry_value(key_path,value_name,value_data,value_type)
-        elif(chose==3):
-            print("[+] Chose mode 3. ")
-            key_path = input("[+] Enter the key path need to delete : ")
-            while (check_registry_key_exists(key_path) == False):
-                print(Fore.RED+"[-] Key path does not  exist."+Style.RESET_ALL)
-                key_path = input("[+] Enter the key path again : ")
-            remove_registry_key(key_path)
-        elif(chose==4):
-            print("[+] Chose mode 4. ")
-            key_path = input("[+] Enter the key path : ")
-            while (check_registry_key_exists(key_path) == False):
-                print(Fore.RED + "[-] Key path does not  exist."+Style.RESET_ALL)
-                key_path = input("[+] Enter the key path again : ")
-            value_name = input("[+] Enter the name of the value :")
-            remove_registry_value(key_path,value_name)
-        else:
-            print("[+] Choose only form 1 --> 4 ")
-        next = input(" Do you want to next (y/n) ?")
-        if(next == "y"):
-            continue
-        else :
-            print(" [->] Exit.")
-            break
+# Hàm để tạo một registry key
+def create_registry_value(key, subkey, value_name, value_data):
+    hkey = wintypes.HKEY()
+    disposition = wintypes.DWORD()
+    # Tạo hoặc mở registry key
+    result = RegCreateKeyEx(
+        key,
+        subkey,
+        0,
+        None,
+        REG_OPTION_NON_VOLATILE,
+        KEY_WRITE,
+        None,
+        ctypes.byref(hkey),
+        ctypes.byref(disposition)
+    )
+
+    if result != 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+
+    # Đặt giá trị cho registry key
+    result = RegSetValueEx(
+        hkey,
+        value_name,
+        0,
+        REG_SZ,
+        value_data,
+        (len(value_data) + 1) * ctypes.sizeof(wintypes.WCHAR)
+    )
+    if result != 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+
+    # Đóng registry key
+    RegCloseKey(hkey)
+    print(f"[+] Registry key '{subkey}' created/updated successfully.")
+#Hàm xóa một key
+def delete_registry_key(hkey, subkey):
+    result = RegDeleteKeyEx(hkey, subkey, KEY_WOW64_64KEY, 0)
+    return result == 0  # ERROR_SUCCESS
+# Hàm xóa giá trị trong Registry
+def delete_registry_value(hkey, subkey, value_name):
+    handle = wintypes.HKEY()
+    result = RegOpenKeyEx(hkey, subkey, 0, KEY_WRITE, ctypes.byref(handle))
+    if result != 0:  # ERROR_SUCCESS
+        return False
+    result = RegDeleteValue(handle, value_name)
+    RegCloseKey(handle)
+    return result == 0  # ERROR_SUCCESS
+def menu():
+    print("[+] 1.Created value. ")
+    print("[+] 2.Created key. ")
+    print("[+] 3.Delete key.")
+    print("[+] 4.Delete value. ")
+def get_hkey():
+    print("[+] 1.HKEY_CURRENT_USER ")
+    print("[+] 2.HKEY_LOCAL_MACHINE ")
+    chose = int(input("[+] Chosse hkey : "))
+    if(chose == 1):
+        hkey = HKEY_CURRENT_USER
+    elif(chose == 2):
+        hkey = HKEY_LOCAL_MACHINE
+    return hkey
 if __name__ == '__main__':
-    run()
-#C:\Users\Administrator\Desktop\Toolmalware\pestudio-9.59\pestudio\pestudio.exe
-#HKCU\Software\Microsoft\Windows\CurrentVersion\Run
+    while True:
+        menu()
+        choose = int(input("Please, choose :"))
+        if(choose == 1):
+            print("[+] You chose to create key ")
+            hkey = get_hkey()
+            subkey = input("[+] Enter subkey : ")
+            while(check_registry_key(hkey,subkey) == False):
+                print("[+] Subkey does not exist.")
+                subkey = input("[+] Enter subkey again  : ")
+            value_name = input("[+] Enter value name :")
+            while(check_registry_value(hkey,subkey,value_name) == True):
+                print("[+] Value already exist.")
+                value_name = input("[+] Enter value name again :")
+            value_data = input("[+] Enter value data :")
+            create_registry_value(hkey,subkey,value_name,value_data)
+        elif(choose==2):
+            print("[+] You chose to create key ")
+            hkey = get_hkey()
+            subkey = input("[+] Enter subkey : ")
+            while (check_registry_key(hkey, subkey) == True):
+                print("[+] Subkey already exist.")
+                subkey = input("[+] Enter subkey again  : ")
+            create_registry_key(hkey,subkey)
+        elif(choose==3):
+            print("[+] You chose to delete key ")
+            hkey = get_hkey()
+            subkey = input("[+] Enter subkey need to delete : ")
+            while (check_registry_key(hkey, subkey) == False):
+                print("[+] Subkey does not exist.")
+                subkey = input("[+] Enter subkey again  : ")
+            delete_registry_key(hkey,subkey)
+            print("[+] Deleted key")
+        elif(choose==4):
+            print("[+] You chose to delete value ")
+            hkey = get_hkey()
+            subkey = input("[+] Enter subkey : ")
+            while (check_registry_key(hkey, subkey) == False):
+                print("[+] Subkey does not exist.")
+                subkey = input("[+] Enter subkey again  : ")
+            value_name = input("[+] Enter value name :")
+            while (check_registry_value(hkey, subkey, value_name) == False):
+                print("[+] Value does not exist.")
+                value_name = input("[+] Enter value name again :")
+            delete_registry_value(hkey,subkey, value_name)
+            print("[+] Deleted value")
+        else:print("Choose 1 --> 4")
+        next = input("[+] What do you want to next (y/n) ? ")
+        if(next == 'n'):
+            break
